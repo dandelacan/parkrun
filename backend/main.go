2cdecad7511a86e675b04ddbf12807c7005dd029
Daniel.Harkin <daniel.harkin@bjss.com>
2023-01-16 15:04:08 +0000

    adjusted cors policy again again

--- backend/main.go
+++ backend/main.go
@@ -60,11 +60,31 @@ func main() {
 		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}),
 	)
 
+	func corsMiddleware(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middleware", r.Method)
+	   
+	   
+		if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers:", "Origin, Content-Type, X-Auth-Token, Authorization")
+		w.Header().Set("Content-Type", "application/json")
+		return
+		}
+	   
+	   
+		next.ServeHTTP(w, r)
+		log.Println("Executing middleware again")
+		})
+	   }
+
 	r := mux.NewRouter()
 	r.Use(cors)
+	r.Use()
 	r.HandleFunc("/users", usersController.ViewUsers)
 	r.HandleFunc("/times", timeRecordController.ViewTimes).Methods("GET")
 	r.HandleFunc("/times", timeRecordController.AddTimes).Methods("POST")
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", corsMiddleware(r))
 }
