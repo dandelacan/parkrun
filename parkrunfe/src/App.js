de8a75e5bfc6a236442cfa6502f2432e04a43ef8
daniel Harkin <daniel.harkin@bjss.com>
2023-01-18 15:39:13 +0000

    removed said slash

--- parkrunfe/src/App.js
+++ parkrunfe/src/App.js
@@ -1,71 +1,71 @@
-import './App.css';
+import "./App.css";
 
-import React, { useRef } from 'react';
-import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
-import ResultContainerPlugin from './ResultContainerPlugin.jsx'
-import Timer from './Timer';
-import GetByDate from './GetByDate';
-
+import React, { useRef } from "react";
+import Html5QrcodePlugin from "./Html5QrcodePlugin.jsx";
+import ResultContainerPlugin from "./ResultContainerPlugin.jsx";
+import Timer from "./Timer";
+import GetByDate from "./GetByDate";
 
 class App extends React.Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
-      decodedResults: [{decodedResult:{decodedText:"12634"}, time:100000}, {decodedResult:{decodedText:"56878"}, time:300000}]
-    }
-    this.childRef = React.createRef()
-   
+      decodedResults: [
+        { decodedResult: { decodedText: "12634" }, time: 100000 },
+        { decodedResult: { decodedText: "56878" }, time: 300000 },
+      ],
+    };
+    this.childRef = React.createRef();
+
     // This binding is necessary to make `this` work in the callback.
     this.onNewScanResult = this.onNewScanResult.bind(this);
-    this.clearTimes =this.clearTimes.bind(this)
+    this.clearTimes = this.clearTimes.bind(this);
   }
-
 
   render() {
     return (
       <div className="App">
         <section className="App-section">
-          <div className="App-section-title"> Html5-qrcode React demo</div>
+          <div className="App-section-title"> Parkrun barcode scanner</div>
           <br />
-          <br /> 
           <br />
-          <Html5QrcodePlugin 
+          <br />
+          <Html5QrcodePlugin
             fps={10}
             qrbox={250}
             disableFlip={false}
-            qrCodeSuccessCallback={this.onNewScanResult}/>
-            <Timer ref={this.childRef} />
-          <ResultContainerPlugin results={this.state.decodedResults} clearTimes={this.clearTimes}/>
-          <GetByDate/>
+            qrCodeSuccessCallback={this.onNewScanResult}
+          />
+          <Timer ref={this.childRef} />
+          <ResultContainerPlugin
+            results={this.state.decodedResults}
+            clearTimes={this.clearTimes}
+          />
+          <GetByDate />
         </section>
       </div>
     );
   }
 
   onNewScanResult(decodedText, decodedResult) {
-    const childElement = this.childRef.current
-    console.log(
-      "App [result]", decodedResult, childElement.state.timeElapsed);
-      
+    const childElement = this.childRef.current;
+    console.log("App [result]", decodedResult, childElement.state.timeElapsed);
+
     // let decodedResults = this.state.decodedResults;
     // decodedResults.push(decodedResult);
     this.setState((state, props) => {
-      
-      state.decodedResults.push({decodedResult: decodedResult, time: childElement.state.timeElapsed});
+      state.decodedResults.push({
+        decodedResult: decodedResult,
+        time: childElement.state.timeElapsed,
+      });
       return state;
     });
   }
 
-
-  clearTimes(){
-    console.log("called clear times")
-    this.setState({decodedResults:[]})
+  clearTimes() {
+    console.log("called clear times");
+    this.setState({ decodedResults: [] });
   }
-
-
- 
 }
 
 export default App;
