f57b61194463b11825c75c3436a8c040775f7152
daniel Harkin <daniel.harkin@bjss.com>
2023-01-18 14:54:59 +0000

    added slash to http endpoint

--- parkrunfe/src/httpFunction.js
+++ parkrunfe/src/httpFunction.js
@@ -1,22 +1,25 @@
-import axios from "axios"
+import axios from "axios";
 
-async function submitTimes(results){
-    const timesArr = []
-    results.forEach(result=>{
-        timesArr.push({Time: result.time.toString(), BarcodeID:result.decodedResult.decodedText})
-    })
+async function submitTimes(results) {
+  const timesArr = [];
+  results.forEach((result) => {
+    timesArr.push({
+      Time: result.time.toString(),
+      BarcodeID: result.decodedResult.decodedText,
+    });
+  });
 
-    var config = {
-        method: 'post',
-        url: 'http://ec2-13-41-189-141.eu-west-2.compute.amazonaws.com:3000/times',
-        headers: { 
-          'Content-Type': 'application/json'
-        },
-        data : JSON.stringify(timesArr)
-      };
+  var config = {
+    method: "post",
+    url: "http://ec2-13-41-189-141.eu-west-2.compute.amazonaws.com:3000/times/",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify(timesArr),
+  };
 
-    const response = await axios(config)
-    return response.status
+  const response = await axios(config);
+  return response.status;
 }
 
-export default submitTimes
+export default submitTimes;
